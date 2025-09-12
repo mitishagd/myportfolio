5a71b013a558c051d80b8d006b03b5fe46f012a0
mitishagd <mitisha.dodderi@gmail.com>
2025-09-12 16:11:12 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import WorkInProgress from './pages/WorkInProgress'
+// import WorkInProgress from './pages/WorkInProgress'
 import Home from  './pages/Home'
 
 const Styled = {
   AppContainer: styled.div`
     overflow-y: auto;
     font-family: Calibri, sans-serif;
-    background-color: #f3fcffff;
   `
 }
 
-const isWorkInProgress = false;
+// const isWorkInProgress = false;
 
 const App = () => {
   
   return (
     <Styled.AppContainer>
-      {isWorkInProgress ? <WorkInProgress /> : <Home />}
+      {/* {isWorkInProgress ? <WorkInProgress /> : <Home />} */}
+      <Home />
     </Styled.AppContainer>
   )
 }
