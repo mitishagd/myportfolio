4b6a721fb13066ee3ec5744f6766321637f6b8bf
mitishagd <mitisha.dodderi@gmail.com>
2025-09-12 17:00:26 -0700

    git config case setting

---
+++ src/pages/Home.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+
+import About from '../components/About.tsx'
+import ProfilePicture from '../components/ProfilePicture.tsx'
+import SocialsSidebar from  '../components/SocialsSidebar.tsx'
+import Experience from '../components/Experience.tsx'
+import Skills from '../components/Skills.tsx'
+import Footer from '../components/Footer.tsx'
+
+const Styled = {
+  SocialsSidebar: styled(SocialsSidebar)`
+
+    `,  
+  AboutContainer: styled.div`
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow-y: auto;
+    margin: 0 10%;
+    `,
+  Intro: styled.div<{_isHovered: boolean}>`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: ${({ _isHovered }) => (_isHovered ? '20px' : '22px')};
+    animation: ${({ _isHovered }) => (!_isHovered && 'fadeIn 0.5s forwards')};
+    `,
+    ProfilePicture: styled(ProfilePicture)`
+      float: right;
+    `,
+    Root: styled.div`
+      
+    `
+}
+
+const introText = "Hi, I am Mitisha Ganesha Dodderi."
+const bioText = "Software Engineer | Passionate about tech improving lives | Avid reader"
+
+const Home = () => {
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        <Styled.Root>
+            <Styled.SocialsSidebar />
+            <Styled.AboutContainer>
+                <Styled.ProfilePicture />
+                <Styled.Intro _isHovered={isHovered} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} > 
+                    {isHovered ? bioText : introText} 
+                </Styled.Intro>
+            </Styled.AboutContainer>
+            <About />
+            <Skills />
+            <Experience />
+            <Footer />
+        </Styled.Root>
+    )
+}
+
+export default Home
---
+++ src/pages/WorkInProgress.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const Styled = {
+  AppContainer: styled.div`
+    text-align: center;
+    background-color: #faf9f1ff;
+    wifth: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    color: #333;
+  `
+}
+
+const WorkInProgress = () => {
+    return (
+        <Styled.AppContainer>
+            Work in Progress . . .
+        </Styled.AppContainer>
+    )
+}
+export default WorkInProgress
