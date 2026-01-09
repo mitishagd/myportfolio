0f0a31d188649bda80b9cefc4cf4d34ad0367640
mitishagd <mitisha.dodderi@gmail.com>
2026-01-08 20:21:01 -0800

    add projects - added cartoon generator

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,36 +1,35 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import email from '../assets/email.svg'
-import github from '../assets/github.svg'
-import linkedin from '../assets/linkedin.svg'
-import medium from '../assets/medium.svg'
-import aws from '../assets/aws.svg'
-import docker from '../assets/docker.svg'
-import django from '../assets/django.svg'
-import graphql from '../assets/graphql.svg'
-import javascript from '../assets/javascript.svg'
-import mongodb from '../assets/mongodb.svg'
-import mysql from '../assets/mysql.svg'
-import nodejs from '../assets/nodejs.svg'
-import python from '../assets/python.svg'
-import react from '../assets/react.svg'
-import typescript from '../assets/typescript.svg'
-import java from '../assets/java.svg'
-import jenkins from '../assets/jenkins.svg'
-import git from '../assets/git.svg'
-import springboot from '../assets/springboot.svg'
-import vercel from '../assets/vercel.svg'
-import html from '../assets/html.svg'
-import css from '../assets/css.svg'
-
+import email from '../assets/icons/email.svg'
+import github from '../assets/icons/github.svg'
+import linkedin from '../assets/icons/linkedin.svg'
+import medium from '../assets/icons/medium.svg'
+import aws from '../assets/icons/aws.svg'
+import docker from '../assets/icons/docker.svg'
+import django from '../assets/icons/django.svg'
+import graphql from '../assets/icons/graphql.svg'
+import javascript from '../assets/icons/javascript.svg'
+import mongodb from '../assets/icons/mongodb.svg'
+import mysql from '../assets/icons/mysql.svg'
+import nodejs from '../assets/icons/nodejs.svg'
+import python from '../assets/icons/python.svg'
+import react from '../assets/icons/react.svg'
+import typescript from '../assets/icons/typescript.svg'
+import java from '../assets/icons/java.svg'
+import jenkins from '../assets/icons/jenkins.svg'
+import git from '../assets/icons/git.svg'
+import springboot from '../assets/icons/springboot.svg'
+import vercel from '../assets/icons/vercel.svg'
+import html from '../assets/icons/html.svg'
+import css from '../assets/icons/css.svg'
 
 const Styled = {
-  SocialIcon: styled.img<{ size?: number, _isScale?: boolean}>`
+  SocialIcon: styled.img<{ size?: number; _isScale?: boolean }>`
     width: 25px;
     height: 25px;
     padding: 5px;
   `,
-  SkillsIcon: styled.img<{ size?: number, _isScale?: boolean}>`
+  SkillsIcon: styled.img<{ size?: number; _isScale?: boolean }>`
     width: 100px;
     height: 100px;
     margin: 10px;
@@ -38,92 +37,90 @@ const Styled = {
   `,
 }
 
-
 export function LinkedInIcon(props: { size?: number; className?: string }) {
-    return <Styled.SocialIcon src={linkedin} alt="" {...props} />
+  return <Styled.SocialIcon src={linkedin} alt="" {...props} />
 }
 
 export function GithubIcon(props: { size?: number; className?: string }) {
-    return <Styled.SocialIcon src={github} alt="" {...props} />
-
+  return <Styled.SocialIcon src={github} alt="" {...props} />
 }
 
 export function MediumIcon(props: { size?: number; className?: string }) {
-    return <Styled.SocialIcon src={medium} alt="" {...props} />
+  return <Styled.SocialIcon src={medium} alt="" {...props} />
 }
 
 export function EmailIcon(props: { size?: number; className?: string }) {
-    return <Styled.SocialIcon src={email} alt="" {...props} />
+  return <Styled.SocialIcon src={email} alt="" {...props} />
 }
 
 export function AwsIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={aws} alt="" {...props} />
+  return <Styled.SkillsIcon src={aws} alt="" {...props} />
 }
 
 export function DockerIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={docker} alt="" {...props} />
+  return <Styled.SkillsIcon src={docker} alt="" {...props} />
 }
 
 export function DjangoIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={django} alt="" {...props} />
+  return <Styled.SkillsIcon src={django} alt="" {...props} />
 }
 
 export function GraphqlIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={graphql} alt="" {...props} />
+  return <Styled.SkillsIcon src={graphql} alt="" {...props} />
 }
 
 export function JavaScriptIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={javascript} alt="" {...props} />
+  return <Styled.SkillsIcon src={javascript} alt="" {...props} />
 }
 
 export function MongoDBIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={mongodb} alt="" {...props} />
+  return <Styled.SkillsIcon src={mongodb} alt="" {...props} />
 }
 
-export function MySQLIcon(props: { size?: number; className?: string;  }) {
-    return <Styled.SkillsIcon src={mysql} alt="" {...props} />
+export function MySQLIcon(props: { size?: number; className?: string }) {
+  return <Styled.SkillsIcon src={mysql} alt="" {...props} />
 }
 
-export function NodeJSIcon(props: { size?: number; className?: string;  }) {
-    return <Styled.SkillsIcon src={nodejs} alt="" {...props} />
+export function NodeJSIcon(props: { size?: number; className?: string }) {
+  return <Styled.SkillsIcon src={nodejs} alt="" {...props} />
 }
 
 export function PythonIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={python} alt="" {...props} />
+  return <Styled.SkillsIcon src={python} alt="" {...props} />
 }
 
 export function ReactIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={react} alt="" {...props} />
+  return <Styled.SkillsIcon src={react} alt="" {...props} />
 }
 
 export function TypeScriptIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={typescript} alt="" {...props} />
+  return <Styled.SkillsIcon src={typescript} alt="" {...props} />
 }
 
 export function JavaIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={java} alt="" {...props} />
-}  
+  return <Styled.SkillsIcon src={java} alt="" {...props} />
+}
 
 export function JenkinsIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={jenkins} alt="" {...props} />
+  return <Styled.SkillsIcon src={jenkins} alt="" {...props} />
 }
 
 export function GitIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={git} alt="" {...props} />
+  return <Styled.SkillsIcon src={git} alt="" {...props} />
 }
 
 export function SpringBootIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={springboot} alt="" {...props} />
+  return <Styled.SkillsIcon src={springboot} alt="" {...props} />
 }
 
 export function VercelIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={vercel} alt=""  {...props} />
+  return <Styled.SkillsIcon src={vercel} alt="" {...props} />
 }
 
 export function HTMLIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={html} alt="" {...props} />
+  return <Styled.SkillsIcon src={html} alt="" {...props} />
 }
 
 export function CSSIcon(props: { size?: number; className?: string }) {
-    return <Styled.SkillsIcon src={css} alt="" {...props} />
+  return <Styled.SkillsIcon src={css} alt="" {...props} />
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import { DjangoIcon, JavaScriptIcon, PythonIcon, ReactIcon, TypeScriptIcon, HTMLIcon, CSSIcon, GitIcon, VercelIcon, AwsIcon, JavaIcon, SpringBootIcon, MongoDBIcon, JenkinsIcon, GraphqlIcon, DockerIcon, NodeJSIcon, MySQLIcon } from "./Icons.tsx";
+import {
+  DjangoIcon,
+  JavaScriptIcon,
+  PythonIcon,
+  ReactIcon,
+  TypeScriptIcon,
+  HTMLIcon,
+  CSSIcon,
+  GitIcon,
+  VercelIcon,
+  AwsIcon,
+  JavaIcon,
+  SpringBootIcon,
+  MongoDBIcon,
+  JenkinsIcon,
+  GraphqlIcon,
+  DockerIcon,
+  NodeJSIcon,
+  MySQLIcon,
+} from './Icons.tsx'
 
 const Styled = {
   Root: styled.div`
@@ -14,32 +33,32 @@ const Styled = {
     font-size: 24px;
     margin-bottom: 80px;
   `,
-};
-
-const Skills = () => {
-    return (
-        <Styled.Root>
-            <Styled.Title>Skills</Styled.Title>
-            <ReactIcon />
-            <TypeScriptIcon />
-            <JavaScriptIcon />
-            <HTMLIcon />
-            <CSSIcon />
-            <NodeJSIcon />
-            <PythonIcon />
-            <DjangoIcon />
-            <JavaIcon />
-            <SpringBootIcon />
-            <GraphqlIcon />
-            <MySQLIcon  />
-            <MongoDBIcon />
-            <JenkinsIcon />
-            <DockerIcon />
-            <GitIcon />
-            <VercelIcon />
-            <AwsIcon />
-        </Styled.Root>
-    )
 }
 
-export default Skills;
+const Skills = () => {
+  return (
+    <Styled.Root>
+      <Styled.Title>Skills</Styled.Title>
+      <ReactIcon />
+      <TypeScriptIcon />
+      <JavaScriptIcon />
+      <HTMLIcon />
+      <CSSIcon />
+      <NodeJSIcon />
+      <PythonIcon />
+      <DjangoIcon />
+      <JavaIcon />
+      <SpringBootIcon />
+      <GraphqlIcon />
+      <MySQLIcon />
+      <MongoDBIcon />
+      <JenkinsIcon />
+      <DockerIcon />
+      <GitIcon />
+      <VercelIcon />
+      <AwsIcon />
+    </Styled.Root>
+  )
+}
+
+export default Skills
